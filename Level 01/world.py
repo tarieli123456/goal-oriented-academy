2e69bd6560f958b929404297891afa4269eedfaa
zuka <zukitokutalazee@icloud.com>
2024-05-14 23:10:57 +0400

    commit

---
+++ Level 01/world.py
@@ -0,0 +1,38 @@
+name = "zuka"
+surname = "bagrati"
+year = "2007"
+text = "me miyvar filmebis yureba da minda viyo profesionali 3D dizainer"
+print(name + " " + surname)
+print(year + " " + text)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
